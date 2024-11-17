6928298a097618f2cee820d23b42ed274f888e2d
Enrico Stara <stara@swissquant.com>
2024-11-17 20:01:36 +0100

    Rename viewport properties

--- src/pixerise/viewport.py
+++ src/pixerise/viewport.py
@@ -3,11 +3,11 @@ from pixerise.canvas import Canvas
 
 class ViewPort:
 
-    def __init__(self, w: float, h: float, d: float, canvas: Canvas):
-        self.w = w
-        self.h = h
-        self.d = d
+    def __init__(self, width: float, height: float, distance: float, canvas: Canvas):
+        self.width = width
+        self.height = height
+        self.distance = distance
         self._canvas = canvas
 
     def canvas_to_viewport(self, x, y):
-        return x * self.w / self._canvas.size[0], y * self.h / self._canvas.size[1], self.d
+        return x * self.width / self._canvas.width, y * self.height / self._canvas.height, self.distance
