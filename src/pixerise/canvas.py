21af3e2fb7077dadb06915979c5744623eb43d52
Enrico Stara <stara@swissquant.com>
2024-11-17 20:00:40 +0100

    Add canvas width and height properties

--- src/pixerise/canvas.py
+++ src/pixerise/canvas.py
@@ -4,11 +4,13 @@ import numpy as np
 class Canvas:
     def __init__(self, size=(800, 600)):
         self.size = size
-        self.grid = np.ones((size[0], size[1], 3), dtype=np.uint8) * 32
-        self._center = (size[0] // 2, size[1] // 2)
+        self.width = size[0]
+        self.height = size[1]
+        self.grid = np.ones((self.width, self.height, 3), dtype=np.uint8) * 32
+        self._center = (self.width // 2, self.height // 2)
 
     def set_pixel(self, x, y, color):
-        if x < 0 or x >= self.size[0] or y < 0 or y >= self.size[1]:
+        if x < 0 or x >= self.width or y < 0 or y >= self.height:
             return
         self.grid[x, y] = color
 
